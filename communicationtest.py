3b9b95c65fc1ad326ba5598c0528e790311971e3
Alicia Esquivel <ace6qv@mail.missouri.edu>
2024-07-14 20:27:11 -0500

    Create communicationtest.py

---
+++ communicationtest.py
@@ -0,0 +1,8 @@
+import bme680
+
+try:
+    sensor = bme680.BME680(bme680.I2C_ADDR_PRIMARY)
+except (RuntimeError, IOError):
+    sensor = bme680.BME680(bme680.I2C_ADDR_SECONDARY)
+
+print("Sensor connected:", sensor is not None)
